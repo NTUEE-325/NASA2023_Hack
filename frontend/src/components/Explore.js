ca01652b862f7e441b4b715543d11adaf8e78a2b
ken <b09901028@ntu.edu.tw>
2023-10-08 22:08:24 +0800

    refactor: delete useless comments and log

--- frontend/src/components/Explore.js
+++ frontend/src/components/Explore.js
@@ -4,24 +4,20 @@ import { Button } from "@mui/material";
 import test_video from "./../assets/01.mp4";
 import * as Tone from "tone";
 import { piano_mapping, bassoon_mapping, clarinet_mapping, contrabass_mapping } from "../constant/pitch";
-import directions from "../constant/direction"
+import { directions } from "../constant/direction"
 
 let prevMs = -1;
 
 let prevSoundMs = 0;
 let nextSoundMs;
+let soundCounter = 0;
 
 const Explore = () => {
   const videoRef = useRef(null);
   const canvasRef = useRef(null);
   const [isplayed, setIsPlayed] = useState(false);
-  const { piano, bassoon, clarinet, contrabass } =
-    useContext(InstrumentContext);
+  const { piano, bassoon, clarinet, contrabass } = useContext(InstrumentContext);
   let reqID = null;
-  let random_duration = 0;
-
-  Tone.Listener.forwardX.value = Math.sin(1.23);
-  Tone.Listener.forwardZ.value = -Math.cos(1.23);
 
   const capture = () => {
     const nowMs = Date.now();
@@ -35,7 +31,6 @@ const Explore = () => {
 
         if (width <= 0 || height <= 0) {
           reqID = window.requestAnimationFrame(capture);
-
           return;
         }
 
@@ -56,35 +51,9 @@ const Explore = () => {
         let x = width/2 - radius - range;
         let y = height/2 - radius - range;
 
-
         const frame = ctx.getImageData(x, y, (2 * radius + 2 * range), (2 * radius + 2 * range));
 
-        // const arr = [];
-        // let count = 0;
-        // let count2 = 0;
-        // for (let i = 0; i < frame.data.length; i = i + 4) {
-        //   if (frame.data[i] > 0 || frame.data[i + 1] > 0 || frame.data[i + 2] > 0 || frame.data[i + 3] > 0) {
-        //     count++;
-        //     if ((frame.data[i] + frame.data[i + 1] + frame.data[i + 2]) > 0) {
-        //       arr[count] = {"data" : (frame.data[i] + frame.data[i + 1] + frame.data[i + 2]), "index" : i};
-        //       count2 ++;
-        //     }
-        //   }
-        // }
-
-        // let max = {"value" : 0, "index" : 0};
-        // for (let i = 0; i < frame.data.length; i++) {
-        //   if (i % 4 == 3) continue;
-        //   if (frame.data[i] > max.value) max = {"value" : frame.data[i], "index" : i};
-        // }
-        // console.log("max ", max);
-        // console.log(frame.data.length);
-        // console.log(frame.data);
-        // console.log(frame.width, frame.height, frame.data.length, count, count2);
-        // console.log(arr);
-        // console.log("arr length check finish")
-
-        // // Calculation
+        /* --------------- Calculation -----------------*/
         let selected = [];
         for (let i = -radius; i <= radius; i++) {
           for (let j = -radius; j <= radius; j++) {
@@ -125,16 +94,19 @@ const Explore = () => {
             }
           }
         }
+        /* --------------- Calculation -----------------*/
 
         if (selected.length > 0) {
           nextSoundMs = Date.now();
           if (prevSoundMs == 0 || nextSoundMs - prevSoundMs > 7/29){
             prevSoundMs = nextSoundMs;
-            random_duration = (Math.random() / 2) + 0.5;
 
+            let direction = directions[soundCounter];
+            soundCounter = (soundCounter + 1) % directions.length;
+
+            console.log(direction);
+            console.log(soundCounter);
             console.log("sound");
-            console.log("next duration = ", random_duration);
-            // console.log(selected);
 
             /* --------------- mapping light to sound -----------------*/
             //
@@ -149,15 +121,13 @@ const Explore = () => {
             // angle to pitch
             // 360 -> (pitch).length
             //
+            /* --------------- mapping light to sound -----------------*/
             const colors = ["r", "g", "b"];
             const color2instrument = {"r": piano, "g": bassoon, "b": clarinet};
             const color2pitch = {"r": piano_mapping, "g": bassoon_mapping, "b": clarinet_mapping};
             const max_brightness = 250;
 
             for (let i = 0; i < Math.min(2, selected.length); i++) {
-              /* ---------------------------------------------------------- */
-              /* ---------------------- R : piano ------------------------- */
-              /* ---------------------------------------------------------- */
               for (let j = 0; j < 3; j++) {
                 let color = colors[j];
   
@@ -174,8 +144,8 @@ const Explore = () => {
                 if (brightness > max_brightness) brightness = max_brightness;
                 let velocity = (brightness / max_brightness) - 0.8;
                 
-                console.log (pitch, y, x, theta, angle_interval);
-                console.log (pitch_numbers);
+                // console.log (pitch, y, x, theta, angle_interval);
+                // console.log (pitch_numbers);
   
                 color2instrument[color].triggerAttackRelease(
                   [color2pitch[color][pitch]],
@@ -184,55 +154,9 @@ const Explore = () => {
                   velocity
                 )
               }
-              // /* ---------------------------------------------------------- */
-              // /* ---------------------- G : piano ------------------------- */
-              // /* ---------------------------------------------------------- */
-              // pitch_numbers = color2pitch["g"].length;
-              // angle_interval = 2 * Math.PI / pitch_numbers;
 
-              // x = selected[i].row_base - radius;
-              // y = selected[i].col_base - radius;
-              // theta = Math.atan2(y, x) + Math.PI;         // Math.atan2(y, x)
-
-              // pitch = Math.floor(theta / angle_interval);
-
-              // brightness = selected[i].r + selected[i].g + selected[i].b;
-              // if (brightness > max_brightness) brightness = max_brightness;
-              // velocity = (brightness / max_brightness) - 0.5;
-              
-              // console.log (pitch, y, x, theta, angle_interval);
-
-              // color2instrument["g"].triggerAttackRelease(
-              //   [color2pitch["g"][pitch]],
-              //   2,
-              //   Tone.now(),
-              //   velocity
-              // )
-              // /* ---------------------------------------------------------- */
-              // /* ---------------------- B : piano ------------------------- */
-              // /* ---------------------------------------------------------- */
-              // pitch_numbers = color2pitch["b"].length;
-              // angle_interval = 2 * Math.PI / pitch_numbers;
-
-              // x = selected[i].row_base - radius;
-              // y = selected[i].col_base - radius;
-              // theta = Math.atan2(y, x) + Math.PI;         // Math.atan2(y, x)
-              // // if (selected[i].y < 0) theta += Math.PI;
-
-              // pitch = Math.floor(theta / angle_interval);
-
-              // brightness = selected[i].r + selected[i].g + selected[i].b;
-              // if (brightness > max_brightness) brightness = max_brightness;
-              // velocity = (brightness / max_brightness) - 0.5;
-              
-              // console.log (pitch, y, x, theta, angle_interval);
-
-              // color2instrument["b"].triggerAttackRelease(
-              //   [color2pitch["b"][pitch]],
-              //   1.01,
-              //   Tone.now(),
-              //   velocity
-              // )
+              Tone.Listener.forwardX.value = Math.sin(1.23);
+              Tone.Listener.forwardZ.value = -Math.cos(1.23);
             }
           }
         }
